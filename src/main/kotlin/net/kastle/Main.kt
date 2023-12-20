936786a40513c1032145d89724fbf336200b7a7e
kyomi <me@kyomi.dev>
2023-12-20 13:46:17 -0300

    feat(AsyncChatListener): change the global message format

--- src/main/kotlin/net/kastle/Main.kt
+++ src/main/kotlin/net/kastle/Main.kt
@@ -2,10 +2,7 @@ package net.kastle
 
 import net.kastle.events.player.AsyncChatListener
 import net.kastle.events.player.PlayerJoinListener
-import org.bukkit.Particle
 import org.bukkit.plugin.java.JavaPlugin
-import kotlin.math.cos
-import kotlin.math.sin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
--- src/main/kotlin/net/kastle/events/player/AsyncChatListener.kt
+++ src/main/kotlin/net/kastle/events/player/AsyncChatListener.kt
@@ -2,6 +2,7 @@ package net.kastle.events.player
 
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -13,8 +14,10 @@ class AsyncChatListener : Listener {
             val content = PlainTextComponentSerializer
                 .plainText()
                 .serialize(message)
-            val format = "[${source.name}] $content"
-            Component.text(format)
+            Component.text()
+                .content("${source.name}: $content")
+                .color(NamedTextColor.GRAY)
+                .build()
         }
     }
 }
