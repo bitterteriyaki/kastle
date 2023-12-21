de899238dbb42e9d4c59cb74806d11f1b3d439f6
kyomi <me@kyomi.dev>
2023-12-20 21:17:29 -0300

    feat(gradle): update dependencies and `gradle`

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.22"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
-    id("xyz.jpenilla.run-paper") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("xyz.jpenilla.run-paper") version "2.2.2"
 }
 
 group = property("group")!!
@@ -20,6 +20,14 @@ bukkit {
     apiVersion = "1.20"
     load = BukkitPluginDescription.PluginLoadOrder.STARTUP
     prefix = "kastle"
+    commands {
+        register("party") {
+            description = "Create a party with your friends."
+            usage = "§c/party [ create | invite | kick | join | leave | list | disband ]"
+            permission = "kastle.party"
+            aliases = listOf("p")
+        }
+    }
 }
 
 java.toolchain {
